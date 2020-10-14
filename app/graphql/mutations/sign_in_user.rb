105b0f35366360a008505b395e5debf6669553db
Priscila Aranha <aranhqg@gmail.com>
2020-10-14 10:09:30 -0300

    Add signin message errors.

--- app/graphql/mutations/sign_in_user.rb
+++ app/graphql/mutations/sign_in_user.rb
@@ -9,13 +9,14 @@ module Mutations
 
 		def resolve(credentials: nil)
       # basic validation
-      return unless credentials
+      return GraphQL::ExecutionError.new("Invalid input: missing credentials") unless credentials
 
       user = User.find_by email: credentials[:email]
 
       # ensures we have the correct user
-      return unless user
-      return unless user.authenticate(credentials[:password])
+      return GraphQL::ExecutionError.new("User not found") unless user
+      return GraphQL::ExecutionError.new("Invalid input: wrong password") unless user.authenticate(credentials[:password])
+      
 
       # use Ruby on Rails - ActiveSupport::MessageEncryptor, to build a token
       crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
@@ -24,6 +25,8 @@ module Mutations
       context[:session][:token] = token
 
       { user: user, token: token }
+    rescue ActiveRecord::RecordInvalid => e
+      GraphQL::ExecutionError.new("Invalid input: #{e.record.errors.full_messages.join(', ')}")
     end
 	end
 end
