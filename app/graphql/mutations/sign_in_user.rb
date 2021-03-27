10337ca1ca5a72783345b6ffd23a1c2be6d1f567
Priscila Aranha <aranhqg@gmail.com>
2021-03-27 16:37:57 -0300

    Fix signing tests.

--- app/graphql/mutations/sign_in_user.rb
+++ app/graphql/mutations/sign_in_user.rb
@@ -9,19 +9,19 @@ module Mutations
 
 		def resolve(credentials: nil)
       # basic validation
-      return GraphQL::ExecutionError.new("Invalid input: missing credentials") unless credentials
+      return GraphQL::ExecutionError.new('Invalid input: missing credentials') unless credentials
 
       user = User.find_by email: credentials[:email]
 
       # ensures we have the correct user
-      return GraphQL::ExecutionError.new("User not found") unless user
-      return GraphQL::ExecutionError.new("Invalid input: wrong password") unless user.authenticate(credentials[:password])
-      
+      return GraphQL::ExecutionError.new('User not found') unless user
+      return GraphQL::ExecutionError.new('Invalid input: wrong password') unless user.authenticate(credentials[:password])
+
 
       # use Ruby on Rails - ActiveSupport::MessageEncryptor, to build a token
       crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
-      token = crypt.encrypt_and_sign("user-id:#{ user.id }")
-      
+      token = crypt.encrypt_and_sign("user-id:#{user.id}")
+
       context[:session][:token] = token
 
       { user: user, token: token }
--- test/graphql/mutations/sign_in_user_test.rb
+++ test/graphql/mutations/sign_in_user_test.rb
@@ -1,43 +1,40 @@
 require 'test_helper'
 
 class Mutations::SignInUserTest < ActiveSupport::TestCase
-  def perform(args = {})
+  def create_user
+    @user = User.create(name: 'Jose', email: 'jose@fab.mil.br', password: '1234')
+    @user.reload
+  end
+
+  def perform(**args)
     Mutations::SignInUser.new(object: nil, field: nil, context: { session: {} }).resolve(args)
   end
 
-  def create_user
-    User.create!(
-      name: 'Test User',
-      email: 'email@example.com',
-      password: '[omitted]',
-    )
-  end
-
   test 'success' do
-    user = create_user
-
-    result = perform(
+    create_user
+    response = perform(
       credentials: {
-        email: user.email,
-        password: user.password
+        email: @user.email,
+        password: @user.password
       }
     )
-
-    assert result[:token].present?
-    assert_equal result[:user], user
+    assert response[:token].present?
+    assert_equal response[:user], @user.reload
   end
 
   test 'failure because no credentials' do
-    assert_nil perform
+    assert_equal GraphQL::ExecutionError.new('Invalid input: missing credentials'), perform
   end
 
-  test 'failure because wrong email' do
+  test 'failure because no user found with email' do
     create_user
-    assert_nil perform(credentials: { email: 'wrong' })
+    assert_equal  GraphQL::ExecutionError.new('User not found'),
+                  perform(credentials: { email: 'email', password: '1234' })
   end
 
   test 'failure because wrong password' do
-    user = create_user
-    assert_nil perform(credentials: { email: user.email, password: 'wrong' })
+    create_user
+    assert_equal  GraphQL::ExecutionError.new('Invalid input: wrong password'),
+                  perform(credentials: { email: @user.email, password: 'wrong' })
   end
 end
