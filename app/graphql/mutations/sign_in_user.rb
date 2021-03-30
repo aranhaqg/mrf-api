d6b106488d86e882a81cf24d012b7054ccd63f4b
Priscila Aranha <aranhaqg@gmail.com>
2021-03-30 00:51:07 -0300

    Fix auth token

--- app/graphql/mutations/sign_in_user.rb
+++ app/graphql/mutations/sign_in_user.rb
@@ -1,32 +1,25 @@
 module Mutations
-	class SignInUser < BaseMutation
-		null true
+  class SignInUser < BaseMutation
+    null true
 
-		argument :credentials, Types::AuthProviderCredentialsInput, required: false
+    argument :credentials, Types::AuthProviderCredentialsInput, required: false
 
-		field :token, String, null: true
-		field :user, Types::UserType, null: true
+    field :token, String, null: true
+    field :user, Types::UserType, null: true
 
-		def resolve(credentials: nil)
-      # basic validation
-      return GraphQL::ExecutionError.new('Invalid input: missing credentials') unless credentials
+    def resolve(credentials: nil)
+      return unless credentials
 
       user = User.find_by email: credentials[:email]
 
-      # ensures we have the correct user
-      return GraphQL::ExecutionError.new('User not found') unless user
-      return GraphQL::ExecutionError.new('Invalid input: wrong password') unless user.authenticate(credentials[:password])
+      return unless user
+      return unless user.authenticate(credentials[:password])
 
-
-      # use Ruby on Rails - ActiveSupport::MessageEncryptor, to build a token
-      crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
-      token = crypt.encrypt_and_sign("user-id:#{user.id}")
+      token = AuthToken.token_for_user(user)
 
       context[:session][:token] = token
 
       { user: user, token: token }
-    rescue ActiveRecord::RecordInvalid => e
-      GraphQL::ExecutionError.new("Invalid input: #{e.record.errors.full_messages.join(', ')}")
     end
-	end
+  end
 end
