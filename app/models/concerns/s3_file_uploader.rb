22ccdefb4532460720f453122de3f178df48822e
Priscila Aranha <aranhqg@gmail.com>
2021-03-30 22:25:53 -0300

    Set aws object to public.

--- app/models/concerns/s3_file_uploader.rb
+++ app/models/concerns/s3_file_uploader.rb
@@ -11,6 +11,7 @@ module S3FileUploader
     object = S3_BUCKET.object(name)
 
     if object.upload_file(file_path)
+      set_public_object(name)
       object.public_url
     else
       file_upload_error
@@ -19,5 +20,17 @@ module S3FileUploader
 
   def file_upload_error
     GraphQL::ExecutionError.new('Error uploading file.')
+  end
+
+  def set_public_object(object_key)
+    AWS_CLIENT.put_object_acl(
+      bucket: 'mrf-pdfs',
+      key: object_key,
+      acl: 'public-read'
+    )
+    return true
+  rescue StandardError => e
+    puts "Error setting bucket ACL: #{e.message}"
+    return false
   end
 end
--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -3,4 +3,6 @@ Aws.config.update({
   credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
 })
 
+AWS_CLIENT = Aws::S3::Client.new(region: 'us-east-1')
+
 S3_BUCKET = Aws::S3::Resource.new.bucket('mrf-pdfs')
